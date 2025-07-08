d1e8b4ba897ad4b7504ccb3a407cb87b5acc6b3d
ManeraKai <manerakai@protonmail.com>
2025-07-08 20:08:55 +0300

    Renamed module names

--- jomla_product_multi_image_type/__manifest__.py
+++ jomla_product_multi_image_type/__manifest__.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Multi Image Types Product",
-    'author': "Jomla",
-    'website': "https://github.com/jomla-ae",
-    'category': 'Tools',
-    'version': '0.1',
-    'depends': ['product_multi_image'],
-    'data': [
-        'views/product_image_views.xml'
-    ],
-    'license': 'LGPL-3'
+    "name": "Multi Image Types Product",
+    "author": "Jomla",
+    "website": "https://github.com/jomla-ae",
+    "category": "Tools",
+    "version": "0.1",
+    "depends": ["jomla_product_multi_image"],
+    "data": ["views/product_image_views.xml"],
+    "license": "LGPL-3",
 }
